828c39c30525623bde93bbce2f3ee8f69eac4c69
渡辺拓馬 <>
2019-10-06 17:27:27 +0900

    aaa

--- Gemfile
+++ Gemfile
@@ -26,6 +26,11 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'bootstrap', '~> 4.3.1'
+
+gem "jquery-rails"
+
+gem 'slim-rails', '3.1.3'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.new
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #UserNotifier.send_signup_email(@user).deliver
+      #redirect_to(@user, :notice => 'User created')
+    else
+      render "index"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :user_type, :title, :content)
+  end
+end
